022b1748c3da5e3337f283305f690cc0c05ac94f
isavelyev76 <etodlajava@gmail.com>
2023-12-24 23:56:12 +0300

    Добавлены тестовые данные

--- lab9pinzh/main.cpp
+++ lab9pinzh/main.cpp
@@ -7,5 +7,6 @@ int main()
     cout << "Laboratory work #9. GIT\n";
     cout << "Variant #10. Catalog\n";
     cout << "Author: Ilya Savelyev\n";
+    cout << "Group: 23pinzh1d\n";
     return 0;
 }
